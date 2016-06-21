44122ac284f62b20ae19e0e818c1bd9b2aec00e6
Austin Bingham <austin@sixty-north.com>
2016-06-21 15:09:09 +0200

    Stated work on convert URI.

--- decktape_io/views.py
+++ decktape_io/views.py
@@ -1,5 +1,8 @@
-from pyramid.response import FileResponse
+from pyramid.response import FileResponse, Response
 from pyramid.view import view_config
+
+import json
+import uuid
 
 
 @view_config(route_name='home')
@@ -8,3 +11,25 @@ def main_view_in_elm(request):
                             request=request,
                             content_type='text/html')
     return response
+
+
+@view_config(route_name='convert',
+             request_method='POST',
+             renderer='json')
+def convert(request):
+    """Call this to ask for the conversion of a URL.
+
+    Responds with a JSON object with an 'job_id' field. This 'job_id' can be
+    used to retrieve the results later.
+    """
+    url = request.json_body['name']
+    job_id = uuid.uuid1()
+    # TODO: Request that conversion of 'url' be done, associated with the job-id for later retrieval.
+
+    results = {
+        'job_id': job_id
+    }
+
+    return Response(
+        body=json.dumps(results),
+        content_type='application/json')
