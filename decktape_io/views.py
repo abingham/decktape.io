c5ed14e6b1301c0a0f2602c6c25daf67fcf6d7f8
Austin Bingham <austin@sixty-north.com>
2016-06-21 10:20:53 +0200

    Fixed some typos.

--- decktape_io/views.py
+++ decktape_io/views.py
@@ -1,9 +1,9 @@
-from pyramic.response import FileResponse
+from pyramid.response import FileResponse
 from pyramid.view import view_config
 
 
 @view_config(route_name='home')
-def main_plot_in_elm(request):
+def main_view_in_elm(request):
     response = FileResponse('elm/index.html',
                             request=request,
                             content_type='text/html')
