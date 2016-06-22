56c8823428a8d52ab1e0796e7e856680270be659
Austin Bingham <austin@sixty-north.com>
2016-06-22 08:43:31 +0200

    Doodling around.

--- elm/Main.elm
+++ elm/Main.elm
@@ -2,8 +2,9 @@ module DecktapeIO (..) where
 
 import Effects
 import Html
-import Html exposing (div, fromElement, Html, hr, h1, node, text)
-import Html.Attributes exposing (href, rel, src)
+import Html exposing (div, fromElement, Html, hr, h1, input, label, node, text)
+import Html.Attributes exposing (class, href, rel, src, type', value)
+import Html.Events exposing (on, targetValue)
 import StartApp
 import Task
 import Bootstrap.Html exposing (..)
@@ -12,6 +13,7 @@ import Effects exposing (Effects)
 
 type alias Model =
   { url : String
+  , submittedUrls : List String
   }
 
 
@@ -22,22 +24,24 @@ noFx model =
 
 initialModel : Model
 initialModel =
-  { url = "http://something.example.com" }
+  { url = "http://something.example.com"
+    , submittedUrls = []
+  }
 
 
 type Action
-  = Something
-  | SomethingElse
+  = SetUrl String
+  | SubmitUrl String
 
 
 update : Action -> Model -> ( Model, Effects Action )
 update action model =
   case action of
-    Something ->
-      model |> noFx
+    SetUrl url ->
+      { model | url = url } |> noFx
 
-    SomethingElse ->
-      model |> noFx
+    SubmitUrl url ->
+      { model | submittedUrls = (url :: model.submittedUrls) } |> noFx
 
 
 stylesheet : String -> Html
@@ -49,11 +53,14 @@ script : String -> Html
 script url =
   node "script" [ src url ] []
 
+submittedUrlsView : Model -> List Html
+submittedUrlsView model =
+  List.map (\u -> row_ [ colMd_ 4 4 4 [ text u ] ]) model.submittedUrls
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   containerFluid_
-    [ stylesheet "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    ([ stylesheet "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
     , stylesheet "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
     , script "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"
     , script "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
@@ -62,9 +69,29 @@ view address model =
             4
             4
             4
-            ([ h1 [] [ text model.url ] ])
+            [ label [ class "control-label pull-right" ] [ text "URL:" ] ]
+        , colMd_
+            8
+            8
+            8
+            [ input
+                [ type' "text"
+                , class "form-control"
+                , value model.url
+                , on "input" targetValue (Signal.message address << SetUrl)
+                ]
+                []
+            ]
         ]
-    ]
+    , row_
+        [ colMd_
+            4
+            4
+            4
+            [ btnDefault' "" { btnParam | label = Just "Convert!" } address (SubmitUrl model.url)
+            ]
+        ]
+    ] ++ (submittedUrlsView model))
 
 
 app : StartApp.App Model
