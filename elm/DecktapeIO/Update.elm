a790eb045e621d9664b97e693e8ec5537760d472
Austin Bingham <austin@sixty-north.com>
2016-09-02 14:23:36 -0400

    Refactored update code.

--- elm/DecktapeIO/Update.elm
+++ elm/DecktapeIO/Update.elm
@@ -4,67 +4,11 @@ import DecktapeIO.Actions exposing (..)
 import DecktapeIO.Effects exposing (noFx)
 import DecktapeIO.Model exposing (..)
 import DecktapeIO.Update.Candidates exposing (..)
-import DecktapeIO.Update.Json exposing (..)
+import DecktapeIO.Update.Submission exposing (..)
 import Effects
 import Effects exposing (Effects)
-import Json.Encode
-import Http
-import Http.Extra exposing (Error, get, jsonReader, post, Response, send, stringReader, withBody, withHeader)
 import List.Extra exposing (replaceIf)
 import Result
-import Task
-
--- Process the result of submitting a URL for conversion.
---
--- This results in a `HandleCompletion` action which will be handled
--- separately.
-
-
-handleSubmissionResults : URL -> Result.Result (Error String) (Response DecktapeIO.Model.Output) -> DecktapeIO.Actions.Action
-handleSubmissionResults source_url result =
-    let
-        r =
-            case result of
-                Result.Ok output ->
-                    Result.Ok output.data
-
-                Result.Err error ->
-                    -- TODO: Handle the various flavors of error: UnexpectedPayload, NetworkError, etc.
-                    Result.Err "Something went wrong!"
-    in
-        HandleCompletion source_url r
-
-
--- Submit a request to convert the presentation at `presentationUrl`.
-
-
-submitUrl : URL -> Effects Action
-submitUrl presentationUrl =
-    let
-        url =
-            Http.url "/convert" []
-
-        reader =
-            jsonReader outputDecoder
-
-        bodyObj =
-            Json.Encode.object [ ( "url", Json.Encode.string presentationUrl ) ]
-
-        body =
-            (Http.string (Json.Encode.encode 2 bodyObj))
-
-        task =
-            post url
-                |> withBody body
-                |> withHeader "Content-type" "application/json"
-                |> send reader stringReader
-    in
-        task
-            |> Task.toResult
-            |> Task.map (handleSubmissionResults presentationUrl)
-            |> Effects.task
-
-
 
 
 -- Central update function.
---
+++ elm/DecktapeIO/Update/Candidates.elm
@@ -0,0 +1,51 @@
+-- Functions for fetching candidates from the server
+
+
+module DecktapeIO.Update.Candidates (..) where
+
+import DecktapeIO.Actions exposing (..)
+import DecktapeIO.Model exposing (URL)
+import DecktapeIO.Update.Json exposing (outputDecoder)
+import Effects exposing (Effects)
+import Http
+import Http.Extra exposing (Error, get, jsonReader, post, Response, send, stringReader, withBody, withHeader)
+import Json.Decode
+import Result
+import Task
+
+
+-- Given a URL, this requests matching candidates from the server.
+-- Ultimately this results in an `UpdateCandidates` action.
+
+
+getCandidates : URL -> Effects Action
+getCandidates source_url =
+    let
+        url =
+            Http.url "/candidates" [ ( "url", source_url ) ]
+
+        reader =
+            jsonReader (Json.Decode.list outputDecoder)
+
+        task =
+            get url
+                |> send reader stringReader
+    in
+        task
+            |> Task.toResult
+            |> Task.map handleCandidates
+            |> Effects.task
+
+
+
+-- This handles the server's response to a request for candidates.
+
+
+handleCandidates : Result.Result (Error String) (Response (List DecktapeIO.Model.Output)) -> DecktapeIO.Actions.Action
+handleCandidates result =
+    case result of
+        Result.Ok candidates ->
+            UpdateCandidates candidates.data
+
+        Result.Err error ->
+            UpdateCandidates []
