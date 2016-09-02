7adc8eb87aa22960fbbd7005f7f84a069603f71d
Austin Bingham <austin@sixty-north.com>
2016-09-02 14:04:26 -0400

    Moved route definitions into separate module.

--- decktape_io/app.py
+++ decktape_io/app.py
@@ -1,17 +1,14 @@
 from pyramid.config import Configurator
 
 from .result_db import make_result_db
+from .routes import configure_routes
 
 
 def make_app(global_config=None, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('convert', '/convert')
-    config.add_route('result', '/result/{file_id}')
-    config.add_route('candidates', '/candidates')
-    config.scan()
+
+    configure_routes(config)
 
     # Add a result-db to each request.
     result_db = make_result_db(settings)
