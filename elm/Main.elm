6ab2c1c5b376a4343bd1182e3f3a60cadc96cfd2
Austin Bingham <austin@sixty-north.com>
2016-09-03 12:45:29 +0200

    Got rid of main/app separation.

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,7 +1,16 @@
 module DecktapeIO exposing (..)
 
-import DecktapeIO.App exposing (app)
+import DecktapeIO.Effects exposing (noFx)
+import DecktapeIO.Model exposing (initialModel, Model)
+import DecktapeIO.Update exposing (update)
+import DecktapeIO.View exposing (view)
+import Html.App as Html
 
 main : Program Never
 main =
-  app
+   Html.program
+    { init = noFx initialModel
+    , view = view
+    , update = update
+    , subscriptions = (\_ -> Sub.none)
+    }
