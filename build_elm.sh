841ce5b39f30d94be0e5face7bc3302f564b4ac3
Austin Bingham <austin@sixty-north.com>
2016-09-05 11:27:15 +0000

    Updated build_elm.sh

--- build_elm.sh
+++ build_elm.sh
@@ -1,2 +1,2 @@
-cd elm
-docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID -p 8000:8000 codesimple/elm:0.16 make Main.elm --output index.html --yes --warn
+cd decktape_io/elm
+elm-make --output decktape_io.js --yes --warn Main.elm
