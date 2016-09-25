92428e99e014e5e4ebdeed1bde5ab76b553777c0
Austin Bingham <austin@sixty-north.com>
2016-09-25 17:03:04 +0200

    Yet more travis work.

--- setup.py
+++ setup.py
@@ -17,9 +17,6 @@ requires = [
     'pyramid_debugtoolbar',
     'python_levenshtein',
     'waitress',
-    ]
-
-tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest',  # includes virtualenv
     'pytest-cov',
@@ -42,9 +39,6 @@ setup(name='decktape_io',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      extras_require={
-          'testing': tests_require,
-      },
       install_requires=requires,
       entry_points="""\
       [paste.app_factory]
