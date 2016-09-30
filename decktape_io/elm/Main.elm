d9effbccd82a049351245c4fb5954bf9d3d3fb7a
Austin Bingham <austin@sixty-north.com>
2016-09-30 14:46:00 +0200

    Even better stuff.

--- decktape_io/elm/Main.elm
+++ decktape_io/elm/Main.elm
@@ -6,13 +6,12 @@ import DecktapeIO.Update exposing (update)
 import DecktapeIO.View exposing (view)
 import Html.App as Html
 import Material
-import Material.Layout
 
 
 main : Program Never
 main =
     Html.program
-        { init = ( initialModel, Material.Layout.sub0 DecktapeIO.Msg.Mdl )
+        { init = ( initialModel, Material.init DecktapeIO.Msg.Mdl )
         , view = view
         , update = update
         , subscriptions = Material.subscriptions DecktapeIO.Msg.Mdl
