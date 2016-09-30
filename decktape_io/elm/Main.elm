4d4a0c179bcb2b602ea1bbcf7ede7179b551931c
Austin Bingham <austin@sixty-north.com>
2016-09-30 13:56:43 +0200

    Sorted out subscriptions thing.

--- decktape_io/elm/Main.elm
+++ decktape_io/elm/Main.elm
@@ -1,18 +1,19 @@
 module DecktapeIO exposing (..)
 
--- import DecktapeIO.Effects exposing (noFx)
 import DecktapeIO.Model exposing (initialModel)
 import DecktapeIO.Msg
 import DecktapeIO.Update exposing (update)
 import DecktapeIO.View exposing (view)
 import Html.App as Html
+import Material
 import Material.Layout
+
 
 main : Program Never
 main =
-   Html.program
-    { init = ( initialModel, Material.Layout.sub0 DecktapeIO.Msg.Mdl )
-    , view = view
-    , update = update
-    , subscriptions = always Sub.none -- Material.Layout.subs DecktapeIO.Msg.Mdl initialModel
-    }
+    Html.program
+        { init = ( initialModel, Material.Layout.sub0 DecktapeIO.Msg.Mdl )
+        , view = view
+        , update = update
+        , subscriptions = Material.subscriptions DecktapeIO.Msg.Mdl
+        }
